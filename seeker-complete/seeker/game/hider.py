0f94f08520c5fb6716c999f5899bc7e5bdec8eb7
kleigh23 <rob22003@byui.edu>
2022-05-27 17:08:17 -0600

    game works just needs cleaned up

--- seeker-complete/seeker/game/hider.py
+++ seeker-complete/seeker/game/hider.py
@@ -1,100 +1,5 @@
 import random
 
-wordlist = ["about", "above", "actor", "acute", "admit"]
-word_to_use = random.choice(wordlist)
-list_of_letter = list(word_to_use)
-tries = int(0)
-
-parachute_man =[
-
-"""\
-
---------
-  __
-/____\\
-\    /
- \  /
-  \/
-  0
- /|\\
- / \\
-
---------
-Five to go!
-""",
-"""\
-
---------
-    
-/____\\
-\    /
- \  /
-  \/
-  0
- /|\\
- / \\
-
---------
-Four to go!
-""",
-"""\
---------
-    
-     
-\    /
- \  /
-  \/
-  0
- /|\\
- / \\
-
---------
-Three to go!
-""",
-"""\
---------
-     
-     
- \  /
-  \/
-  0
- /|\\
- / \\
-
---------
-Two to go!
-""",
-
-"""\
-
---------
-     
-  \/
-  0
- /|\\
- / \\
-
---------
-ONE to go!
-""",
-
-"""\
-
---------
- 
-  x
- /|\\
- / \\
-
---------
-Too bad!!
-""", 
-
-"""\
-YOU WIN!!!!
-"""\
-]
-
 
 class Hider:
     """The drawing of the parachute man. 
@@ -112,8 +17,11 @@ class Hider:
         Args:
             self (Hider): An instance of Hider.
         """
-        self._location = word_to_use
-        self._distance = parachute_man[tries]
+        wordlist = ["about", "above", "actor", "acute", "admit"]
+        self._location = random.choice(wordlist)
+        self._list_of_letter = list(self._location)
+        self._word_completion = "_" * len(self._location)
+        self._distance = 0
     
     def get_hint(self):
         """Gets a hint for the seeker.
@@ -127,21 +35,124 @@ class Hider:
         Returns:
             string: A hint for the seeker.
         """
+        parachute_man =["""\
+
+            --------
+              __
+            /____\\
+            \    /
+             \  /
+              \/
+               0
+              /|\\
+              / \\
+
+            --------
+            Five to go!
+            """,
+
+            """\
+
+            --------
+                
+            /____\\
+            \    /
+             \  /
+              \/
+               0
+              /|\\
+              / \\
+
+            --------
+            Four to go!
+            """,
+
+            """\
+
+            --------
+                
+                
+            \    /
+             \  /
+              \/
+               0
+              /|\\
+              / \\
+
+            --------
+            Three to go!
+            """,
+
+            """\
+
+            --------
+                
+                
+            \  /
+             \/
+              0
+             /|\\
+             / \\
+
+            --------
+            Two to go!
+            """,
+
+            """\
+
+            --------
+                
+            \/
+             0
+            /|\\
+            / \\
+
+            --------
+            ONE to go!
+            """,
+
+            """\
+
+            --------
+            
+             x
+            /|\\
+            / \\
+
+            --------
+            Too bad!!
+            """,
+
+            "You did it!!!!"]
+
         hint = "Time to type something."
-        if self._distance == parachute_man[tries]:
+        if self._distance == 0:
             print(f"{parachute_man[0]}")
+            print(self._word_completion)
             hint = "There's still 5 chances"
-        elif self._distance == parachute_man[1]:
+        elif self._distance == 1:
+            print(f"{parachute_man[1]}")
+            print(self._word_completion)
             hint = "There's still 4 chances"
-        elif self._distance ==parachute_man[2]:
+        elif self._distance == 2:
+            print(f"{parachute_man[2]}")
+            print(self._word_completion)
             hint = "There's still 3 chances"
-        elif self._distance == parachute_man[3]:
+        elif self._distance == 3:
+            print(f"{parachute_man[3]}")
+            print(self._word_completion)
             hint = "There's still 2 chances"
-        elif self._distance == parachute_man[4]:
+        elif self._distance == 4:
+            print(f"{parachute_man[4]}")
+            print(self._word_completion)
             hint = "There's still 1 chances"
-        elif self._distance == parachute_man[5]:
+        elif self._distance == 5:
+            print(f"{parachute_man[5]}")
+            print(self._word_completion)
             hint = "Got to restart!"
-        elif self._distance == parachute_man[6]:
+        elif self._distance == 6:
+            print(f"{parachute_man[6]}")
+            print(self._word_completion)
             hint = "You won already!"
         return hint
 
@@ -179,7 +190,8 @@ class Hider:
         Returns:
             boolean: True if the hider was found; false if otherwise.
         """
-        if self._distance == parachute_man[6]:
+        if self._word_completion == self._location:
+            print("you won the game!")
             return True
         
     def watch_seeker(self, seeker):
@@ -191,12 +203,17 @@ class Hider:
         Args:
             self (Hider): An instance of Hider.
         """
-        if seeker in list_of_letter:
+        if seeker.get_guess() in self._list_of_letter:
+            word_as_list = list(self._word_completion)
+            indices = [i for i, letter in enumerate(self._location) if letter == seeker.get_guess()]
+            for index in indices:
+                word_as_list[index] = seeker.get_guess()
+            self._word_completion = "".join(word_as_list)
             print("Good guess!")
         else:
             print("nope")
-            # tries+=1
-            # return
+            self._distance += 1
+            
 
 
     
